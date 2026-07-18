50449374aae3bd6f39b7aab83adbbcc72eace7e6
Lithiyasree <r.lithiyasree12@gmail.com>
2026-07-18 22:08:12 +0530

    Fix GitHub Pages routing using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import { ScrollToTop } from './components/ScrollToTop.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
