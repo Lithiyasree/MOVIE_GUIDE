62108ef0cfe823caa410c6e1095e3e7f5209169b
Lithiyasree <r.lithiyasree12@gmail.com>
2026-07-18 22:34:15 +0530

    Prepare project for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import { ScrollToTop } from './components/ScrollToTop.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <ScrollToTop />
     <App />
-  </HashRouter>
+  </BrowserRouter>
 )
